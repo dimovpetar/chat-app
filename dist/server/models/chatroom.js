75be2de90455c3cf8223c82c5db10063e14a448c
dimovpetar <pdimov461@gmail.com>
2018-01-28 14:15:30 +0200

    fixed .gitignore

--- dist/server/models/chatroom.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var mongoose_1 = require("mongoose");
-var chatroom_1 = require("../schemas/chatroom");
-exports.ChatRoom = mongoose_1.model('ChatRoom', chatroom_1.ChatRoomSchema);
-//# sourceMappingURL=chatroom.js.map
--- dist/shared/interfaces/chatroom.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Update;
-(function (Update) {
-    Update[Update["AddUser"] = 0] = "AddUser";
-    Update[Update["RemoveUser"] = 1] = "RemoveUser";
-    Update[Update["Title"] = 2] = "Title";
-})(Update = exports.Update || (exports.Update = {}));
-//# sourceMappingURL=chatroom.js.map
