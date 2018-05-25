246afcf0944f4dd58e91c2ecda3f5c07871ac0a2
dimovpetar <pdimov461@gmail.com>
2018-05-25 14:08:57 +0300

    removed dotenv from db

--- server/db.ts
+++ server/db.ts
@@ -1,11 +1,10 @@
 import * as Sequelize from 'sequelize';
-import * as dotenv from 'dotenv';
+// import * as dotenv from 'dotenv';
 
 export class Database {
     public sequelize: Sequelize.Sequelize;
 
     constructor() {
-        dotenv.load({path: '.env'});
         let db;
         const user = process.env.MYSQL_USER;
         const pwrd = process.env.MYSQL_PWRD;
@@ -15,7 +14,7 @@ export class Database {
         } else {
             db =  process.env.MYSQL_PROD;
         }
-    //    console.log(user, pwrd, host, db);
+
         this.sequelize = new Sequelize(db, user, pwrd, {
             host: host,
             dialect: 'mysql',
@@ -30,7 +29,7 @@ export class Database {
 
         this.sequelize.authenticate()
         .then(() => {
-          console.log('Connection has been established successfully.');
+          console.log('Connection to database has been established successfully.');
         })
         .catch(err => {
           console.error('Unable to connect to the database:', err);
@@ -40,41 +39,4 @@ export class Database {
 }
 
 export default new Database();
-// export class Database {
-//     public connection: mysql.Connection;
 
-//     setConfig(config) {
-//         this.connection = mysql.createConnection(config);
-//         console.log('mysql configured');
-//         this.init();
-//     }
-
-//     init() {
-//         console.log('mysql initialised');
-//     }
-
-
-//     query(sql, args = null) {
-//         return new Promise((resolve, reject) => {
-//             this.connection.query(sql, args, (err, rows) => {
-//                 if (err) {
-//                     return reject(err);
-//                 }
-//                 resolve(rows);
-//             });
-//         });
-//     }
-
-//     close() {
-//         return new Promise( ( resolve, reject ) => {
-//             this.connection.end(err => {
-//                 if (err) {
-//                     return reject(err);
-//                 }
-//                 resolve();
-//             } );
-//         } );
-//     }
-// }
-
-// export default new Database();
